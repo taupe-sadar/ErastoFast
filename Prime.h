830a2d71636344e83aaf3fcd0cac5e84dc4a65a7
Remi Bertin <remiber@free.fr>
2013-10-17 22:11:08 +0000

    Add header for test (forgotten)

---
+++ Prime.h
@@ -0,0 +1,3 @@
+void init_sieve( int sieveSize );
+void processSieve();
+
--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 
 int main( int  argc, char ** argv )
 {
-  init_sieve();
+  init_sieve(5000);
   processSieve();
   return 0;
 }
